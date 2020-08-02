a625435d6ed41246bbee63bec3cdefe22a5b2499
Morari Inga <ingamorari462@gmail.com>
2020-08-02 19:21:27 +0300

    Add seven six and seven section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,13 @@
-$(document).ready(function () {
+const validateForm = () => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const email = re.test(String($("#email").val()).toLowerCase());
+  const name = !!$("#name").val();
+  const message = $("#message").val().length > 1;
+
+  $(".submit-btn").prop("disabled", !(email && name && message));
+};
+
+$(document).ready(() => {
   $("#scroll-button").click(() => {
     $([document.documentElement, document.body]).animate(
       {
@@ -6,5 +15,10 @@ $(document).ready(function () {
       },
       600
     );
+  });
+  validateForm();
+
+  $("#name, #email, #message, #phone").on("input", () => {
+    validateForm();
   });
 });
